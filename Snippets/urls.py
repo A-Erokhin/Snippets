5edd4528533a26fe6ea2b6f0678960e92dda8294
A-Erokhin <a-erkhin@yandex.ru>
2022-11-25 12:47:39 +0300

    admin

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('', views.index_page, name='home'),
@@ -14,6 +16,7 @@ urlpatterns = [
     path('logout', views.logout_page, name='logout'),
     path('registration', views.registration, name='registration'),
     path('comment/add', views.comment_add, name="comment_add"),
+    path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
